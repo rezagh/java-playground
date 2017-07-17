aef38e8ea407c98cbb8cf808e65c44735d4dca07
Reza <nahangeabi1>
2017-07-17 15:23:06 +1000

    streams

---
+++ JavaLambda/src/stream/StreamTest.java
@@ -0,0 +1,36 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+
+
+/*
+ * http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
+ * http://www.codejava.net/java-core/collections/java-8-stream-terminal-operations-examples 
+ * http://www.codejava.net/java-core/collections/java-stream-aggregate-functions-examples-intermediate-operations
+ */
+
+
+public class StreamTest {
+	
+	@Test
+	public void string_contact(){
+
+		String a = Arrays.stream(new String[] {"1", "2", "3"})
+	    .map(n -> n + "\n")
+	    .collect(Collectors.joining(""));
+		System.out.println(a);
+	}
+	
+	@Test
+	public void counter(){
+	
+		long out = Arrays.stream(new int[] {1,2,3,4,5}).count();
+		System.out.println(out);
+		
+		
+	}
+}
